25de2d37f9eee43bbbd1cc13c9bc6d9c2032cfb0
vivek rawat <vivekrawat7534@gmail.com>
2025-07-05 17:51:06 +0530

    feat/added-query-parameters

--- main.py
+++ main.py
@@ -39,3 +39,26 @@ async def read_file(file_path: str, path2: str):
 async def read_file(file_path: str):
     return {"file_path": file_path}
 
+
+## query parameters
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+# optional parameters
+@app.get("/queryitems/")
+async def read_item(skip: int=0, limit : int|None = None):
+    if limit:
+        return fake_items_db[skip: skip+limit]
+    else:
+        return fake_items_db[skip:]
+    
+@app.get("/querybol/")
+async def  read_item(q: bool = False):
+    if q:
+        return "the query parameter was true" # will run on q=1,yes,true, True
+    else:
+        return "query paramet was false" # q=0,no,false,False
